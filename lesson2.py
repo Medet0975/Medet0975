73a443edbe46c827a1972f753a1d90921d9d698c
Medet0975 <medetabzaparov@gmail.com>
2022-06-29 10:12:49 +0600

    week 2

---
+++ lesson2.py
@@ -0,0 +1,85 @@
+# print(3)
+
+# a = [2, 3, 4]
+##s = 'String'
+# a.reverse()
+# s.lower()
+# print(a)
+# print(s.lower())
+
+# if
+
+# num1 = int(input('num1: '))
+# num2 = int(input('num2: '))
+#
+# if num1 > num2:
+#     print("{0} grater {1}".format(num1, num2))
+# elif num2 > num1:
+#     print("{0} grater {1}".format(num2, num1))
+
+# num1 = int(input("num1: "))
+# num2 = int(input("num2: "))
+# operator = input("operator: ")
+#
+# if operator == "+":
+#     result = num2 + num1
+#     print("result = {}".format(result))
+#
+# elif operator == "-":
+#     result = num1 - num2
+#     print("result = {}".format(result))
+#
+# elif operator == "*":
+#     result = num1 * num2
+#     print("result = {}".format(result))
+#
+# else:
+#     result = num1 / num2
+#     print("result = {}".format(result))
+
+
+# lists = [1, 3, 5, 6]
+#
+# if 3 in lists:
+#    print("Hello")
+
+# or and условные операторы or выдает если хотя бы одно условие выполняется. and выполняет при соблюдении обоих условий
+
+# lists = [18, 22, 25, 6]
+# if 22 in lists and 25 in lists:
+#    print("yes")
+# list_2 = list ()
+# lists =list(range(1,1001))
+# for i in lists:
+#    if i % 59 == 0 and i % 5 == 0 and i % 15 == 0:
+#       list_2.append(i)
+# print(list_2)
+
+# gradus = input("Введите температуру: ")
+# operator = input("Оператор: ")
+# if operator == "temp" :
+#    print(gradus * 9 / 5 - 32, "по Фаренгейту")
+#
+# elif operator == "far" :
+#    print(gradus * 5 / 9 - 32, "по Цельсию")
+# num1 = 3
+# num2 = 4
+# if num1 > num2:
+#    print("num1 is smaller than num2")
+# print("not in if")
+
+# for i in [1,2,3,4,5,6]:
+#    print(i)
+
+# for i in range(11):
+#    print(2, "*", i, "=", (2 * i))
+
+# num = int(input("enter a number: "))
+
+# slovo = "Python"
+# for i in range(1, len(slovo)+1):
+#     print(slovo)
+
+
+for i in range(1+5, 7+11):
+    print("name")
